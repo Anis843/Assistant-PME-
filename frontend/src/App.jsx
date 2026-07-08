99115d34a900b8fb0883e174f5ac66ce47b26f1f
Anis Ouaret <ouaretanis08@gmail.com>
2026-07-08 14:06:59 +0100

    initialisation du projet Assistant pme

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <p className=" bg-amber-50">Je suis la </p>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  );
+}
+
+export default App;
